2ac4ee18f4664735381f60a69f8fc21fa6af2189
Florian MARIN <flomarin88@gmail.com>
2023-01-21 23:05:23 +0100

    2022 - Day 05 - Part 2

--- src/2022/day5.ts
+++ src/2022/day5.ts
@@ -5,8 +5,8 @@ interface CratesPlan {
 }
 
 interface MoveDescription {
-  count: number,
-  from: number,
+  count: number
+  from: number
   to: number
 }
 
@@ -16,44 +16,53 @@ export class Day5 extends Day {
   }
 
   solveForPartOne(input: string): string {
+    return this.solve(input, this.doSingleMoves)
+  }
+
+  solveForPartTwo(input: string): string {
+    return this.solve(input, this.doComplexMoves)
+  }
+
+  solve(
+    input: string,
+    move: (cratesPlan: CratesPlan, desc: MoveDescription) => void,
+  ) {
     const puzzle = input.split('\n\n')
-    const cratesPlan = this.parseCratesPlan(puzzle[0])
+    const cratesPlan = this.getInitialPlan(puzzle[0])
     const procedures = puzzle[1].split('\n')
-    procedures.forEach(procedure => {
+    procedures.forEach((procedure) => {
       if (procedure !== '') {
-        this.doMove(cratesPlan, this.getMove(procedure))
+        move(cratesPlan, this.getMoveDescription(procedure))
       }
     })
     return this.heads(cratesPlan)
   }
 
-  solveForPartTwo(input: string): string {
-    return ''
-  }
-  
-  parseCratesPlan(input: string): CratesPlan {
+  getInitialPlan(input: string): CratesPlan {
     const lines = input.split('\n')
-    const chars = lines.map(line => Array.from(line))
-    const rotation = chars[0].map((val, index) => chars.map(row => row[index]).reverse())
+    const chars = lines.map((line) => Array.from(line))
+    const rotation = chars[0].map((val, index) =>
+      chars.map((row) => row[index]).reverse(),
+    )
     return rotation.reduce((plan, line) => {
       if (line[0] !== ' ') {
-        plan[Number(line.shift())] = line.filter(l => l !== ' ')
+        plan[Number(line.shift())] = line.filter((l) => l !== ' ')
       }
       return plan
     }, {} as CratesPlan)
   }
-  
+
   // move 2 from 2 to 1
-  getMove(input: string): MoveDescription {
+  getMoveDescription(input: string): MoveDescription {
     const split = input.split(' ')
     return {
       count: Number(split[1]),
       from: Number(split[3]),
-      to: Number(split[5])
+      to: Number(split[5]),
     }
   }
-  
-  doMove(cratesPlan: CratesPlan, desc: MoveDescription) {
+
+  doSingleMoves(cratesPlan: CratesPlan, desc: MoveDescription) {
     for (let i = 0; i < desc.count; i++) {
       const crate = cratesPlan[desc.from].pop()
       if (crate) {
@@ -61,10 +70,20 @@ export class Day5 extends Day {
       }
     }
   }
-  
+
+  doComplexMoves(cratesPlan: CratesPlan, desc: MoveDescription) {
+    const fromStack = cratesPlan[desc.from]
+    const cratesToMove = fromStack.splice(fromStack.length - desc.count)
+    if (cratesToMove) {
+      cratesPlan[desc.to].push(...cratesToMove)
+    }
+  }
+
   heads(cratesPlan: CratesPlan): string {
-    return Object.keys(cratesPlan).map(key => {
-      return cratesPlan[Number(key)].pop()
-    }).join('')
+    return Object.keys(cratesPlan)
+      .map((key) => {
+        return cratesPlan[Number(key)].pop()
+      })
+      .join('')
   }
 }
--- test/2022/day5.test.ts
+++ test/2022/day5.test.ts
@@ -26,13 +26,32 @@ describe('2022 - Day 5', () => {
       const input =
         '    [D]    \n' + '[N] [C]    \n' + '[Z] [M] [P]\n' + ' 1   2   3 '
 
-      const result = day.parseCratesPlan(input)
+      const result = day.getInitialPlan(input)
 
       assert.deepEqual(result, {
         1: ['Z', 'N'],
         2: ['M', 'C', 'D'],
         3: ['P'],
       })
+    })
+  })
+
+  describe('Part 2', () => {
+    it('rearranges the crates - multiple crates at a time', () => {
+      const input =
+        '    [D]    \n' +
+        '[N] [C]    \n' +
+        '[Z] [M] [P]\n' +
+        ' 1   2   3 \n' +
+        '\n' +
+        'move 1 from 2 to 1\n' +
+        'move 3 from 1 to 3\n' +
+        'move 2 from 2 to 1\n' +
+        'move 1 from 1 to 2\n'
+
+      const result = day.solveForPartTwo(input)
+
+      assert.equal(result, 'MCD')
     })
   })
 })
