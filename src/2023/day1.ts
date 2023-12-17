7f0678ccc955bf24183ec77dab725d160c8bf694
Florian MARIN <flomarin88@gmail.com>
2023-12-17 17:05:56 +0100

    feat(2023-Day01) Part 2

--- src/2023/day1.ts
+++ src/2023/day1.ts
@@ -29,6 +29,42 @@ export class Day1 extends Day {
   }
 
   solveForPartTwo(input: string): number | string {
-    return 0
+    const regex = /(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/g
+    
+    let counter: number = 0
+    const lines = input.split('\n')
+    lines.forEach((line) => {
+      if (line !== '') {
+        const matches = Array.from(line.matchAll(regex))
+        if (matches.length > 0) {
+          counter += this.toNumber(matches[0][1]) * 10 + this.toNumber(matches[matches.length - 1][1])
+        }
+      }
+    })
+    return counter
+  }
+  
+  toNumber(word: string): number {
+    const mapping: { [key: string]: number} = {
+      'one': 1,
+      'two': 2,
+      'three': 3,
+      'four': 4,
+      'five': 5,
+      'six': 6,
+      'seven': 7,
+      'eight': 8,
+      'nine': 9,
+      '1': 1,
+      '2': 2,
+      '3': 3,
+      '4': 4,
+      '5': 5,
+      '6': 6,
+      '7': 7,
+      '8': 8,
+      '9': 9,
+    }
+    return mapping[word]
   }
 }
